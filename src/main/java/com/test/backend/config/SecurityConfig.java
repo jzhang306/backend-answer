ebd62dbb832c0ed7b8134c94b26e93985b8cc8de
jzhang306 <zhangjun20522@gmail.com>
2025-02-07 23:40:27 +0100

    Login request while user is logged in, which is set by default in Spring security.

--- src/main/java/com/test/backend/config/SecurityConfig.java
+++ src/main/java/com/test/backend/config/SecurityConfig.java
@@ -3,10 +3,14 @@ import com.test.backend.service.AccountService;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+
+
 
 @Configuration
 public class SecurityConfig {
@@ -22,8 +26,8 @@ public class SecurityConfig {
                 .formLogin(conf->conf
                         .loginProcessingUrl("/api/auth/login")
                         .successHandler((request, response, authentication) -> {
-                                response.setContentType("application/json;charset=utf-8");
-                                response.getWriter().write("Login success\n");
+                            response.setContentType("application/json;charset=utf-8");
+                            response.getWriter().write("Login successful\n");
                         })
                         .failureHandler((request, response, exception) -> {
                             response.setContentType("application/json;charset=utf-8");
@@ -33,7 +37,23 @@ public class SecurityConfig {
                 )
                 .logout(conf -> conf
                         .logoutUrl("/api/auth/logout")
+                                .logoutSuccessHandler((request, response, authentication) -> {
+                                    response.setContentType("application/json;charset=utf-8");
+                                    // without login, you cannot log out!
+                                    if (authentication == null || authentication instanceof AnonymousAuthenticationToken) {
+                                        response.setStatus(401);
+                                        response.getWriter().write("Logout failed: Not logged in\n");
+                                    } else {
+                                        response.getWriter().write("Logout success\n");
+                                    }
+                                })
                         )
+                .exceptionHandling(conf->conf
+                        .authenticationEntryPoint((request, response, authException) -> {
+                            response.setContentType("application/json;charset=utf-8");
+                            response.setStatus(401);
+                            response.getWriter().write("Authentication Failed\n");
+                        }))
                 .userDetailsService(accountService)
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
